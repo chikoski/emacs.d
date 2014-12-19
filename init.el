066b92ca43642b4900814cb78603d3efb1ec93d6
N.Shimizu <chikoski@gmail.com>
2014-12-19 09:43:50 +0900

    updated

--- init.el
+++ init.el
@@ -3,6 +3,9 @@
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
+;; start server for emacsclient use
+(server-start)
+
 ;; an intractive function to kill all visited bufferes except *scratch*
 (defun kill-all-buffer ()
   (interactive)
@@ -26,10 +29,27 @@
 
 ;; hide menu 
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(display-time-mode t)
  '(tool-bar-mode nil)
  '(transient-mark-mode t))
-(custom-set-faces)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(web-mode-comment-face ((t (:foreground "#D9333F"))))
+ '(web-mode-css-at-rule-face ((t (:foreground "#FF7F00"))))
+ '(web-mode-css-pseudo-class-face ((t (:foreground "#FF7F00"))))
+ '(web-mode-css-rule-face ((t (:foreground "#A0D8EF"))))
+ '(web-mode-doctype-face ((t (:foreground "#82AE46"))))
+ '(web-mode-html-attr-name-face ((t (:foreground "#C97586"))))
+ '(web-mode-html-attr-value-face ((t (:foreground "#82AE46"))))
+ '(web-mode-html-tag-face ((t (:foreground "#E6B422" :weight bold))))
+ '(web-mode-server-comment-face ((t (:foreground "#D9333F")))))
 
 
 ;;Color
@@ -65,10 +85,8 @@
           64 66 68 70 72 74 76 78 80 82 84 86 88 90
           92 94 96 98 100 102 104 106 108 110 112 114 116 120))
 
-
 ;; bind C-h delete-backward-char
 (global-set-key "\C-h" 'delete-backward-char)
-
 
 ;; upcase-regtion / downcase-region を使えるように
 (put 'upcase-region 'disabled nil)
@@ -88,7 +106,6 @@
         js2-bounce-indent-flag nil)
 )
   
-
 ;; graphviz-dot-mode.el
 (autoload 'graphviz-dot-mode "graphviz-dot-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.dot$" . graphviz-dot-mode))
@@ -96,6 +113,7 @@
 ;; markdown-mode.el
 (autoload 'markdown-mode "markdown-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+
 ;; yaml-mode.el
 (autoload 'yaml-mode "yaml-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
@@ -125,9 +143,10 @@
      do (kill-buffer buffer)))
 
 ;; rust-mode
-(autoload 'rust-mode "rust-mode" nil t)
-(setq auto-mode-alist
-      (cons '("\\.rs$'" . rust-mode) auto-mode-alist))
+(when (require 'rust-mode nil t)
+  (setq auto-mode-alist
+        (cons '("\\.rs$'" . rust-mode) auto-mode-alist))
+  (setq rust-indent-offset 2))
 
 ;; web-mode
 (autoload 'web-mode "web-mode" nil t)
@@ -137,32 +156,16 @@
 	    (setq web-mode-html-offset   2)
 	    (setq web-mode-css-offset    2)
 	    (setq web-mode-script-offset 2)))
-(custom-set-faces
- '(web-mode-doctype-face
-   ((t (:foreground "#82AE46"))))                          ; doctype
- '(web-mode-html-tag-face
-   ((t (:foreground "#E6B422" :weight bold))))             ; 要素名
- '(web-mode-html-attr-name-face
-   ((t (:foreground "#C97586"))))                          ; 属性名など
- '(web-mode-html-attr-value-face
-   ((t (:foreground "#82AE46"))))                          ; 属性値
- '(web-mode-comment-face
-   ((t (:foreground "#D9333F"))))                          ; コメント
- '(web-mode-server-comment-face
-   ((t (:foreground "#D9333F"))))                          ; コメント
- '(web-mode-css-rule-face
-   ((t (:foreground "#A0D8EF"))))                          ; cssのタグ
- '(web-mode-css-pseudo-class-face
-   ((t (:foreground "#FF7F00"))))                          ; css 疑似クラス
- '(web-mode-css-at-rule-face
-   ((t (:foreground "#FF7F00"))))                          ; cssのタグ
-)
+
 
 
 ;; start json-mode when the file name is manifest.webapp
 (require 'json-mode nil t)
 (add-to-list 'auto-mode-alist '("manifest.webapp" . json-mode))
 (add-to-list 'auto-mode-alist '("package.manifest" . json-mode))
+(add-hook 'json-mode-hook
+          (lambda ()
+            (setq js-indent-level 2)))
 
 
 ;; helm
@@ -224,32 +227,15 @@
 (add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
 
 ;; magit
-(require 'magit)
 (set-variable 'magit-emacsclient-executable "/usr/local/bin/emacsclient")
+(require 'magit)
 
 ;; typescript
 (require 'typescript)
 
 ;;; apache-mode
 (autoload 'apache-mode "apache-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.htaccess\\'"   . apache-mode))
-
-;; el-get
-(setq el-get-dir "~/.emacs.d/lisp")
-(require 'el-get)
-(el-get 'sync)
-
-(setq el-get-sources
-      '(
-        (:name review-mode
-               :type github
-               :description "Emacs lisp helper mode for ReVIEW"
-               :pkgname "kmuto/review-el")
-        ))
-
-
-;; review-mode
-(require 'review-mode)
+(add-to-list 'auto-mode-alist '("\\.htaccess\\'" . apache-mode))
 
 ;; scss-mode
 (require 'scss-mode)
@@ -258,3 +244,6 @@
 ;; emacsclient
 (unless (server-running-p)
   (server-start))
+
+;; disable dialog box, which force Emacs to be abort
+(setq use-dialog-box nil)
